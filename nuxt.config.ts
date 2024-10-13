a7bcea095599fc6abd4ba4585a2a9a1400e6c30f
loicmaes <production@maesloic.fr>
2024-10-13 10:53:02 +0200

    :rocket: Install and config default dependencies
    Signed-off-by: loicmaes <production@maesloic.fr>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "nuxt-mailer",
+    "nuxt-scheduler",
+    "@nuxt/eslint",
+    "@nuxtjs/google-fonts",
+    "shadcn-nuxt",
+  ],
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Poppins: true,
+    },
+  },
+  tailwindcss: {
+    cssPath: ["assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config.ts",
+    exposeConfig: false,
+    viewer: true,
+  },
+});
